537020d0897d449f2194db991ac7ace1a708c505
nencyvadadoriya <vadadoriyanency8@gmail.com>
2024-11-29 16:26:49 +0530

    Add files via upload

---
+++ test-1.js
@@ -0,0 +1,86 @@
+// ans-1)
+//==> find out the area of triangle
+        var b=10;
+        var h=30;
+        var ans=1/2*b*h; 
+        console.log("area of triangle = "+ans);
+//=>output =150
+
+// ans-2)
+//==>convet to tempercatures to and form celsius , faherenhit
+
+//==> convert celsius to Fahrenheit
+        var c=10;
+        var ans=(c*9/5)+32; //(0°C × 9/5) + 32 = 32°F
+        console.log("celsius to Fahrenheit   = "+ans);
+//output=50;
+
+//==> convert Fahrenheit to celsius
+        var f=10;
+        var ans=(f-32)*5/9; 
+        console.log("Fahrenheit to celsius  = "+ans);
+//=> output= -12.222222222222223
+
+//==>3)
+//==> find out area of Rectangle
+        var l=40;
+        var w=80;
+        var ans=l*w; 
+        console.log(" area of the Rectangle = "+ans);
+//output=3200;
+
+//==>4)
+//==> find the area of circle
+        var c=40;
+        var ans=3.14*c;
+        console.log("area of circle = "+ans);
+//output=125.601
+
+//==>5)
+        var f=10;
+        var ans=f*12;
+        console.log("Feet into inches = "+ans );
+// output=120
+
+//=>6)
+
+//1) a*a - b*b =(a-b)(a+b)
+        var a=20;
+        var b=40;
+        var ans=(a-b)*(a+b);
+        console.log("a*a - b*b =(a-b)(a+b) = "+ans);
+//output=1200
+
+//2)(a-b)*2 = a*a -2ab +b*b;
+        var a=20;
+        var b=40;
+        var ans=(a+b)*2;
+        console.log("(a-b)*2 = a*a -2ab +b*b = "+ans);
+// output=120
+
+//3)(a+b+c)*2 = a*a +b*b + c*c +2ab + 2ac + 2bc
+        var a=20;
+        var b=40;
+        var c=30;
+        var ans=(a+b+c)*2;
+        console.log("a*a +b*b + c*c +2ab + 2ac + 2bc =  " + ans);
+//output= 180
+
+//4)(a-b-c)*2 = a*a +b*b + c*c -2ab -2ac - 2bc
+        var a=20;
+        var b=30;
+        var c=40;
+        var ans=(a-b-c)*2;
+        console.log("a*a +b*b + c*c -2ab -2ac - 2bc = "+ans);
+//output =-100;
+
+//5)(a-b)*3 = a*a*a+ 3a*ab+ 3ab*2 - b*3;
+        var a=20;
+        var b=10;
+        var ans=(a-b)*3;
+        console.log("a*a*a+ 3a*ab+ 3ab*2 - b*3 = "+ans);
+//output=30; 
+
+
+
+
