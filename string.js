29db264bc54a72981da11bb7c426b4be0ee5c805
nency <vadadoriyanency8@gmail.com>
2025-01-03 18:56:18 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,110 @@
+//string
+
+//defination--
+//collection of character
+
+//define--
+// "hello", 'hello', ` hello"world" ` , ${hello}
+
+// let data="hello";
+// console.log(data);
+
+// let data='hello world'
+// console.log(data);
+//  console.log(data.length);
+//  console.log(data[0]);
+
+
+//string litreal (templet litreal):=>  ` ` in one sentence used to "" or '' 
+
+// let data='nency ," vadadoriya" ';
+// console.log(data);
+
+let data="      JavaScript is the world's most popular programming language."
+// console.log(data);
+// console.log(data.length);
+// let a= data.slice(0,10);
+// console.log(a);
+
+//substring ()
+// let a=data.substring(0,10);
+// console.log(a);
+
+//substr()
+//  let a=data.substr(10,20);
+//  console.log(a);
+
+//split()
+// let a=data.split("");
+// console.log(a);
+
+// let a=data.split(" ");
+// console.log(a);
+
+//trim()=>extra remove space of starting and ending
+// let a=data.trim();
+// console.log(a);
+
+// touppercase ()
+// let a=data.toUpperCase();
+// console.log(a);
+
+//tolowercase
+// let a=data.toLowerCase();
+// console.log(a);
+
+//String length
+// String charAt()
+// String charCodeAt()
+// String at()
+// String [ ]
+
+//String concat() => joins two or more strings 
+// let a = "Hello";
+// let b = "World";
+// let c = a.concat(" ", b);
+// console.log(c);
+
+// String trimStart() => removes whitespace only from the start of a string.
+// let a = "     Hello World!";
+// let b = a.trimStart();
+// console.log(b);
+
+// String trimEnd() =>  removes whitespace only from the end of a string.
+// let a = "Hello World!     ";
+// let b = a.trimEnd();
+// console.log(b);
+
+// String padStart() =>To pad a number, convert the number to a string first.
+// let a = "nency";
+// let b = a.padStart(6, "hello");
+// console.log(b);
+
+// String padEnd() =>method pads a string from the end
+// let a = "nency";
+// let b = a.padStart(10, "x");
+// console.log(b);
+
+// String repeat()
+// String replace()
+// String replaceAll()
+
+
+//math function 
+// math.floor()=>sammlest value retun krega point ke bad ki nahi
+// let a=Math.floor(10.111)
+// console.log(a);
+
+//math.ceil()=. decimal value in largest value return
+// let a=Math.ceil(20.90)
+// console.log(a);
+
+//math.random()=>random number ko print krega
+// let a=Math.random()
+// console.log(a);
+
+//data function()=> show the date time day month year
+// let a=new Date();
+// console.log(a);
+
+
