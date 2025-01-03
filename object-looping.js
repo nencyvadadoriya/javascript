e167742f45a9951026b0137490e54c5c1a977a7f
nency <vadadoriyanency8@gmail.com>
2025-01-03 18:54:31 +0530

    3-1-25

---
+++ object-looping.js
@@ -0,0 +1,53 @@
+//object per array
+// let person = {
+//     name: "nency",
+//     age: 18,
+//     email: "nencyvadadoriya@gmail.com",
+//     hobby : ["eating","music","coding"],
+//     address : "150,radhe park , katargam ,surat ",
+//     zipcode: 395010
+// };
+
+// var a=[1,2,3,4]
+// console.log(a)
+// console.log(person.address);
+// console.log(person.zipcode);
+// console.log(person.hobby[1]);
+
+//for in ==>object // array
+// for(let i in person){
+//     console.log(i, ":= ", person[i])
+// }
+
+//for in loop for array
+// for(let data of a){
+//     console.log(data)
+// }
+
+//for of loop => array
+// var fruits =["mango","chiku","apple"]
+// console.log(fruits);
+
+// for(let i of fruits){
+//     console.log(fruits[0])
+// }
+
+let fruits =[
+    {id : 1 , name : "apple"},
+    {id : 2 , name : "banana"},
+    {id : 3 , name : "chiku"},
+    {id : 4 , name : "dregen"},
+    {id : 5 , name : "grepes"},
+    {id : 6 , name : "lichi"},
+    {id : 7 , name : "ciwi"},
+    {id : 8 , name : "watermelon"},
+    {id : 9 , name : "pinepple"},
+    {id : 10 , name : "strawberry"}
+]
+console.log(fruits);
+
+function random(){
+        let num = Math.floor(Math.random)*10
+
+}
+
