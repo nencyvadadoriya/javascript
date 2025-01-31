e057056d382ba83e224cddf6ffbda47b7f177d09
nency <vadadoriyanency8@gmail.com>
2025-01-31 13:51:28 +0530

    ajax.js

--- ajax.js
+++ ajax.js
@@ -1,6 +1,16 @@
 // AJAX = Asynchronous JavaScript And XML. 
+
 // defination--
 // AJAX (Asynchronous JavaScript and XML) is a technique in JavaScript that allows web pages to fetch or send data to a server without reloading the page.
+
+
+//=>Key Features of AJAX
+//1. Asynchronous Communication
+//2. Uses XMLHttpRequest (XHR) or Fetch API
+//3. Supports Multiple Data Formats
+//4. Reduces Server Load
+//5. Enhances User Experience
+
 
 document.querySelector('button').addEventListener('click', () => {
     let xhr = new XMLHttpRequest();
